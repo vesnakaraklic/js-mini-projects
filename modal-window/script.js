911a9f08e50fdcbef3007bd76161f31f2545ce85
Vesna Karaklić <vesna.karaklic.97@gmail.com>
2023-01-13 14:48:46 +0100

    esc keypress event added

--- modal-window/script.js
+++ modal-window/script.js
@@ -20,3 +20,9 @@ btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (event) {
+if(event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+}
+})
