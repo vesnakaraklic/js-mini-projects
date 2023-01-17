57d9e63d20f9102739748b024ab18701bed0d8ac
Vesna Karaklić <vesna.karaklic.97@gmail.com>
2023-01-17 12:38:03 +0100

    optimized code

--- pig-game/script.js
+++ pig-game/script.js
@@ -2,56 +2,78 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-let currentScore0 = document.getElementById('current--0');
-let currentScore1 = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNewGame = document.querySelector('.btn--new');
-let totalScore0 = 0; 
-let totalScore1 = 0;
-let countOne = 0;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0; 
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+init();
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 btnRollDice.addEventListener('click', function() {
-    const randNumber = Math.trunc(Math.random() * 6) +1;
-    diceEl.classList.remove('hidden');
+    if(playing) {
+        const randNumber = Math.trunc(Math.random() * 6) +1;
 
-    diceEl.src = `dice-${randNumber}.png`;
-    
-    if(randNumber !== 1) {
-        countOne % 2 === 0 || countOne === 0 ? totalScore0 += randNumber : totalScore1 += randNumber;
-        currentScore0.textContent = totalScore0;
-        currentScore1.textContent = totalScore1;
-    } else {
-        countOne++;
-        currentScore0.textContent = 0;
-        currentScore1.textContent = 0;
-        totalScore0 = 0;
-        totalScore1 = 0;
-    }
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${randNumber}.png`;
+        
+        if(randNumber !== 1) {
+            currentScore += randNumber;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    } 
 })
 
 btnHold.addEventListener('click', function() {
-    countOne % 2 === 0 || countOne === 0 ? score0El.textContent = Number(score0El.textContent) + totalScore0 : score1El.textContent = Number(score1El.textContent) + totalScore1;
-    countOne++;
-    currentScore0.textContent = 0;
-    currentScore1.textContent = 0;
-    totalScore0 = 0;
-    totalScore1 = 0;
+
+    if(playing) {
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        if(scores[activePlayer] >= 100) {
+            playing = false;
+            diceEl.classList.add('hidden');
+
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            switchPlayer();
+        }
+    }
 })
 
-btnNewGame.addEventListener('click', function() {
-diceEl.classList.add('hidden');
-countOne = 0;
-currentScore0.textContent = 0;
-currentScore1.textContent = 0;
-score0El.textContent = 0;
-score1El.textContent = 0;
-totalScore0 = 0
-totalScore1 = 0;
-
-})
+btnNewGame.addEventListener('click', init)
